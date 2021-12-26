52b6a7abf23e530b78efd1b62b30ae2e411d916f
SylarWu <702005971@qq.com>
2021-12-26 19:12:08 +0800

    【序列预测】删除序列预测以及相应的Unet模型

--- model/config/unet_config.py
+++
@@ -1,6 +0,0 @@
-class UNetConfig(object):
-    def __init__(self):
-        self.n_axis = 3
-        self.init_channels = 16
-        self.pool_sizes = [14, 7, 2]
-        self.bottleneck_factor = 4
