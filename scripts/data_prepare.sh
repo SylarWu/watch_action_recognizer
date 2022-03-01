f1b40841177581f19a75c10014c19e1a46a9b887
SylarWu <702005971@qq.com>
2022-03-01 16:37:33 +0800

    【脚本】数据预处理自动生成脚本优化

--- scripts/data_prepare.sh
+++ scripts/data_prepare.sh
@@ -1,10 +1,15 @@
+datasource_path="/data/wuxilei/watch_action_recognizer/transform_source"
+output_path="/data/wuxilei/watch_action_recognizer"
+seq_len=224
+normalize=true
+
 # 基于normal策略对数据切分
 for ((i = 0; i < 5; ++i));
 do
   echo "==============================================================================================================="
-  echo "基于normal_$i 策略对数据源进行切分"
-  python ../data_process/sensor_data_preprocess.py --input '/data/wuxilei/watch_action_recognizer/transform_source' \
-  --output '/data/wuxilei/watch_action_recognizer' --strategy normal_$i --length 224 --normalize true
+  echo "基于normal_${i}策略对数据源进行切分"
+  python ../data_process/sensor_data_preprocess.py --input ${datasource_path} --output ${output_path} \
+  --strategy normal_${i} --length ${seq_len} --normalize ${normalize}
   echo "==============================================================================================================="
 done
 
@@ -12,15 +17,15 @@ done
 for ((i = 0; i < 10; ++i));
 do
   echo "==============================================================================================================="
-  echo "基于user_$i 策略对数据源进行切分"
-  python ../data_process/sensor_data_preprocess.py --input '/data/wuxilei/watch_action_recognizer/transform_source' \
-  --output '/data/wuxilei/watch_action_recognizer' --strategy user_$i --length 224 --normalize true
+  echo "基于user_${i}策略对数据源进行切分"
+  python ../data_process/sensor_data_preprocess.py --input ${datasource_path} --output ${output_path} \
+  --strategy user_${i} --length ${seq_len} --normalize ${normalize}
   echo "==============================================================================================================="
 done
 
 # 基于shuffle策略对数据切分
 echo "==============================================================================================================="
 echo "基于shuffle策略对数据源进行切分"
-python ../data_process/sensor_data_preprocess.py --input '/data/wuxilei/watch_action_recognizer/transform_source' \
---output '/data/wuxilei/watch_action_recognizer' --strategy shuffle --length 224 --normalize true
+  python ../data_process/sensor_data_preprocess.py --input ${datasource_path} --output ${output_path} \
+  --strategy shuffle --length ${seq_len} --normalize ${normalize}
 echo "==============================================================================================================="
