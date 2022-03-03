e9bb68fc308d1b9f154a14b479f0165729e7aae7
SylarWu <702005971@qq.com>
2022-03-03 21:41:55 +0800

    【参数配置】修改默认参数配置

--- basic_config.py
+++ basic_config.py
@@ -3,10 +3,15 @@ import os
 
 class BasicConfig(object):
     def __init__(self):
+        # model/head/strategy
+        self.model_name = 'resnet101'
+        self.head_name = 'span_cls'
+        self.strategy_name = 'span_cls'
+
         # 数据预处理脚本config
-        self.datasource_path = os.path.join('/data/wuxilei/watch_action_recognizer', 'data_source', 'transform_source')
-        self.dataset_path = os.path.join('/data/wuxilei/watch_action_recognizer', 'data_source')
-        self.preprocess_strategy = 'user_1'
+        self.datasource_path = os.path.join('/data/wuxilei/watch_action_recognizer', 'transform_source')
+        self.dataset_path = os.path.join('/data/wuxilei/watch_action_recognizer')
+        self.preprocess_strategy = 'shuffle'
         self.seq_len = 224
         self.train_test_ratio = [0.8, 0.2]
 
@@ -18,18 +23,17 @@ class BasicConfig(object):
         self.weight_decay = 1e-4
         self.save_epoch = 10
         self.eval_epoch = 1
-        self.check_point_path = os.path.join('/data/wuxilei/watch_action_recognizer', 'log')
+        self.check_point_path = os.path.join('/data/wuxilei/watch_action_recognizer/log', '%s-%s' % (
+            self.model_name, self.preprocess_strategy
+        ))
         self.use_gpu = True
         self.gpu_device = "0"
-        self.eval_batch_size = 16
-
-        # model/head/strategy
-        self.model_name = 'resnet101'
-        self.head_name = 'span_cls'
-        self.strategy_name = 'span_cls'
+        self.eval_batch_size = 64
 
         # 测试超参
-        self.test_batch_size = 16
-        self.model_path = os.path.join('/data/wuxilei/watch_action_recognizer', 'log', 'ResNet1D_SpanClassifier_final')
+        self.test_batch_size = 64
+        self.model_path = os.path.join('/data/wuxilei/watch_action_recognizer/log', '%s-%s' % (
+            self.model_name, self.preprocess_strategy
+        ), 'ResNet1D_SpanClassifier_final')
 
         self.n_classes = 18
