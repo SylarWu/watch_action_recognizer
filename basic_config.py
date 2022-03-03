fb24a4eef232094a79a73cc4ea024dbde2f4e3db
SylarWu <702005971@qq.com>
2022-03-03 16:06:06 +0800

    【参数修改】在默认参数加中数据集路径修改为服务器存储路径

--- basic_config.py
+++ basic_config.py
@@ -4,14 +4,13 @@ import os
 class BasicConfig(object):
     def __init__(self):
         # 数据预处理脚本config
-        self.datasource_path = os.path.join(os.getcwd(), 'data_source', 'transform_source')
-        self.dataset_path = os.path.join(os.getcwd(), 'data_source')
+        self.datasource_path = os.path.join('/data/wuxilei/watch_action_recognizer', 'data_source', 'transform_source')
+        self.dataset_path = os.path.join('/data/wuxilei/watch_action_recognizer', 'data_source')
         self.preprocess_strategy = 'user_1'
         self.seq_len = 224
         self.train_test_ratio = [0.8, 0.2]
 
         # 训练超参
-        self.batch_size = 64
         self.train_batch_size = 64
         self.num_epoch = 100
         self.opt_method = 'adamw'
@@ -19,8 +18,9 @@ class BasicConfig(object):
         self.weight_decay = 1e-4
         self.save_epoch = 10
         self.eval_epoch = 1
-        self.check_point_path = os.path.join(os.getcwd(), 'log')
+        self.check_point_path = os.path.join('/data/wuxilei/watch_action_recognizer', 'log')
         self.use_gpu = True
+        self.gpu_device = "0"
         self.eval_batch_size = 16
 
         # model/head/strategy
@@ -30,6 +30,6 @@ class BasicConfig(object):
 
         # 测试超参
         self.test_batch_size = 16
-        self.model_path = os.path.join(os.getcwd(), 'log', 'ResNet1D_SpanClassifier_final')
+        self.model_path = os.path.join('/data/wuxilei/watch_action_recognizer', 'log', 'ResNet1D_SpanClassifier_final')
 
         self.n_classes = 18
