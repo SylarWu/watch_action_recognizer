2876f35a34afc9bb278b1bea631bd0984876f04b
SylarWu <702005971@qq.com>
2022-03-22 18:40:53 +0800

    【自动化脚本】参数修改，不保存中间结果

--- scripts/vit-normalize.sh
+++ scripts/vit-normalize.sh
@@ -6,7 +6,7 @@ python="/home/wuxilei/anaconda3/envs/torch/bin/python3.8"
 dataset_path="/data/wuxilei/watch_action_recognizer/"
 check_point_path="/data/wuxilei/watch_action_recognizer/log/"
 
-cuda=0
+cuda=1
 
 for model_name in resnet18 resnet34 resnet50 resnet101
 do
@@ -18,7 +18,7 @@ do
         preprocess_strategy=normal_${i}
         echo "===============================================基于${preprocess_method}方法${preprocess_strategy}策略对模型进行训练==============================================="
         CUDA_VISIBLE_DEVICES=${cuda} ${python} training.py --dataset_path ${dataset_path} \
-        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 \
+        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 --is_normalize true \
         --train_batch_size 256 --eval_batch_size 256 --num_epoch 1000 --opt_method "adamw" \
         --lr_rate 5e-4 --lr_rate_adjust_epoch 50 --lr_rate_adjust_factor 0.5 --weight_decay 1e-4 \
         --save_epoch 50 --eval_epoch 1 --patience 50 \
@@ -26,7 +26,7 @@ do
         --model_name ${model_name} --head_name "span_cls" --strategy_name "span_cls"
         echo "===============================================基于${preprocess_method}方法${preprocess_strategy}策略对模型进行测试==============================================="
         CUDA_VISIBLE_DEVICES=${cuda} ${python} testing.py --dataset_path ${dataset_path} \
-        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 \
+        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 --is_normalize true \
         --check_point_path ${check_point_path} --use_gpu true --gpu_device ${cuda} \
         --model_name ${model_name} --head_name "span_cls" --strategy_name "span_cls" \
         --test_batch_size 256
@@ -37,7 +37,7 @@ do
         preprocess_strategy=user_${i}
         echo "===============================================基于${preprocess_method}方法${preprocess_strategy}策略对模型进行训练==============================================="
         CUDA_VISIBLE_DEVICES=${cuda} ${python} training.py --dataset_path ${dataset_path} \
-        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 \
+        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 --is_normalize true \
         --train_batch_size 256 --eval_batch_size 256 --num_epoch 1000 --opt_method "adamw" \
         --lr_rate 5e-4 --lr_rate_adjust_epoch 50 --lr_rate_adjust_factor 0.5 --weight_decay 1e-4 \
         --save_epoch 50 --eval_epoch 1 --patience 50 \
@@ -45,7 +45,7 @@ do
         --model_name ${model_name} --head_name "span_cls" --strategy_name "span_cls"
         echo "===============================================基于${preprocess_method}方法${preprocess_strategy}策略对模型进行测试==============================================="
         CUDA_VISIBLE_DEVICES=${cuda} ${python} testing.py --dataset_path ${dataset_path} \
-        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 \
+        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 --is_normalize true \
         --check_point_path ${check_point_path} --use_gpu true --gpu_device ${cuda} \
         --model_name ${model_name} --head_name "span_cls" --strategy_name "span_cls" \
         --test_batch_size 256
@@ -56,15 +56,15 @@ do
         preprocess_strategy=shuffle_${i}
         echo "===============================================基于${preprocess_method}方法${preprocess_strategy}策略对模型进行训练==============================================="
         CUDA_VISIBLE_DEVICES=${cuda} ${python} training.py --dataset_path ${dataset_path} \
-        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 \
+        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 --is_normalize true \
         --train_batch_size 256 --eval_batch_size 256 --num_epoch 1000 --opt_method "adamw" \
         --lr_rate 5e-4 --lr_rate_adjust_epoch 50 --lr_rate_adjust_factor 0.5 --weight_decay 1e-4 \
-        --save_epoch 1001 --eval_epoch 1 --patience 50 \
+        --save_epoch 50 --eval_epoch 1 --patience 50 \
         --check_point_path ${check_point_path} --use_gpu true --gpu_device ${cuda} \
         --model_name ${model_name} --head_name "span_cls" --strategy_name "span_cls"
         echo "===============================================基于${preprocess_method}方法${preprocess_strategy}策略对模型进行测试==============================================="
         CUDA_VISIBLE_DEVICES=${cuda} ${python} testing.py --dataset_path ${dataset_path} \
-        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 \
+        --preprocess_method ${preprocess_method} --preprocess_strategy ${preprocess_strategy} --seq_len 224 --is_normalize true \
         --check_point_path ${check_point_path} --use_gpu true --gpu_device ${cuda} \
         --model_name ${model_name} --head_name "span_cls" --strategy_name "span_cls" \
         --test_batch_size 256
