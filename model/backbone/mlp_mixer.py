9166dde2e172efe914d0368240618aa0905f080b
SylarWu <702005971@qq.com>
2022-03-27 17:17:57 +0800

    【MLP-Mixer】对acc和gyr数据分别做embedding然后进行拼接

--- model/backbone/mlp_mixer.py
+++ model/backbone/mlp_mixer.py
@@ -50,35 +50,40 @@ class MLPMixerBlock(nn.Module):
 
 
 class MLPMixer(nn.Module):
-    def __init__(self, seq_len, patch_size, n_channels, hidden_dim, num_layers=4, expansion_factor=4, dropout=0.1):
+    def __init__(self, seq_len, patch_size, acc_axis, gyr_axis, hidden_dim, num_layers=4, expansion_factor=4, dropout=0.1):
         super(MLPMixer, self).__init__()
         assert seq_len % patch_size == 0
-        self.n_channels = n_channels
-        self.num_patches = seq_len // patch_size
+        self.acc_axis = acc_axis
+        self.gyr_axis = gyr_axis
+        self.seq_len = seq_len
         self.patch_size = patch_size
         self.hidden_dim = hidden_dim
 
-        self.embedding = nn.Linear(self.n_channels * self.patch_size, hidden_dim, bias=False)
+        self.embedding_acc = nn.Linear(self.acc_axis * self.patch_size, hidden_dim, bias=False)
+        self.embedding_gyr = nn.Linear(self.gyr_axis * self.patch_size, hidden_dim, bias=False)
 
         self.encoders = nn.Sequential(
-            *[MLPMixerBlock(self.num_patches, hidden_dim, expansion_factor, dropout) for _ in range(num_layers)]
+            *[MLPMixerBlock(self.seq_len // self.patch_size * 2, hidden_dim, expansion_factor, dropout)
+              for _ in range(num_layers)]
         )
 
-        nn.init.xavier_normal_(self.embedding.weight)
+        nn.init.xavier_normal_(self.embedding_acc.weight)
+        nn.init.xavier_normal_(self.embedding_gyr.weight)
 
         self.norm = nn.LayerNorm(hidden_dim)
 
     def _pickup_patching(self, batch_data):
         # batch_size, n_channels, seq_len
-        batch_size = batch_data.size(0)
-        batch_data = batch_data.view(batch_size, self.n_channels, self.num_patches, self.patch_size)
+        batch_size, n_channels, seq_len = batch_data.size()
+        batch_data = batch_data.view(batch_size, n_channels, seq_len // self.patch_size, self.patch_size)
         batch_data = batch_data.permute(0, 2, 1, 3)
-        batch_data = batch_data.contiguous().view(batch_size, self.num_patches, self.n_channels * self.patch_size)
+        batch_data = batch_data.contiguous().view(batch_size, seq_len // self.patch_size, n_channels * self.patch_size)
         return batch_data
 
-    def forward(self, batch_data):
-        batch_data = self._pickup_patching(batch_data)
-        batch_data = self.embedding(batch_data)
+    def forward(self, accData, gyrData):
+        accData, gyrData = self._pickup_patching(accData), self._pickup_patching(gyrData)
+        accData, gyrData = self.embedding_acc(accData), self.embedding_gyr(gyrData)
+        batch_data = torch.cat([accData, gyrData], dim=1)
         batch_data = self.encoders(batch_data)
         batch_data = self.norm(batch_data)
         batch_data = torch.mean(batch_data, dim=1)
@@ -113,7 +118,8 @@ def mlp_mixer(model_name: str, config: MLPMixerConfig):
         # Large
         config.num_layers = 24
         config.hidden_dim = 1024
-    return MLPMixer(seq_len=config.seq_len, patch_size=config.patch_size, n_channels=config.n_channels,
+    return MLPMixer(seq_len=config.seq_len, patch_size=config.patch_size,
+                    acc_axis=config.acc_axis, gyr_axis=config.gyr_axis,
                     hidden_dim=config.hidden_dim, num_layers=config.num_layers,
                     expansion_factor=config.expansion_factor,
                     dropout=config.dropout)
--- model/config/mlp_mixer_config.py
+++ model/config/mlp_mixer_config.py
@@ -2,7 +2,8 @@ class MLPMixerConfig(object):
     def __init__(self):
         self.seq_len = 224
         self.patch_size = 16
-        self.n_channels = 3 + 3
+        self.acc_axis = 3
+        self.gyr_axis = 3
         self.hidden_dim = 256
         self.num_layers = 6
         self.expansion_factor = 4
